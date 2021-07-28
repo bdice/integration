c2df142087e8fa86b4cc01fd19b0b57cd8eb93f5
jolorunyomi <joseph.cloudengineer@gmail.com>
2021-07-28 08:31:58 -0500

    Added Test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,242 +1,92 @@
 #!/bin/bash
+# Copyright (c) 2019-2021, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Any failing command will set EXITCODE to non-zero
+set -e           # abort the script on error, this will change for running tests (see below)
+set -o pipefail  # piped commands propagate their error
+set -E           # ERR traps are inherited by subcommands
+trap "EXITCODE=1" ERR
+
+NUMARGS=$#
+ARGS=$*
+THISDIR=$(cd $(dirname $0);pwd)
+CUGRAPH_ROOT=$(cd ${THISDIR}/..;pwd)
+GTEST_ARGS="--gtest_output=xml:${CUGRAPH_ROOT}/test-results/"
+DOWNLOAD_MODE=""
+EXITCODE=0
+
+export RAPIDS_DATASET_ROOT_DIR=${CUGRAPH_ROOT}/datasets
+
+# FIXME: consider using getopts for option parsing
+function hasArg {
+    (( ${NUMARGS} != 0 )) && (echo " ${ARGS} " | grep -q " $1 ")
+}
+
+# Add options unique to running a "quick" subset of tests here:
+#  - pass --subset flag to download script to skip large downloads
+#  - filter the "huge" dataset tests
+if hasArg "--quick"; then
+    echo "Running \"quick\" tests only..."
+    DOWNLOAD_MODE="--subset"
+    GTEST_FILTER="--gtest_filter=-hibench_test/Tests_MGSpmv_hibench.CheckFP32_hibench*:*huge*"
+else
+    echo "Running all tests..."
+    # FIXME: do we still need to always filter these tests?
+    GTEST_FILTER="--gtest_filter=-hibench_test/Tests_MGSpmv_hibench.CheckFP32_hibench*"
+fi
+
+if hasArg "--skip-download"; then
+    echo "Using datasets in ${RAPIDS_DATASET_ROOT_DIR}"
+else
+    echo "Download datasets..."
+    cd ${RAPIDS_DATASET_ROOT_DIR}
+    bash ./get_test_data.sh ${DOWNLOAD_MODE}
+fi
+
+if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
+    cd ${CUGRAPH_ROOT}/cpp/build
+else
+    export LD_LIBRARY_PATH="$WORKSPACE/ci/artifacts/cugraph/cpu/conda_work/cpp/build:$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+    cd $WORKSPACE/ci/artifacts/cugraph/cpu/conda_work/cpp/build
+fi
+
+# Do not abort the script on error from this point on. This allows all tests to
+# run regardless of pass/fail, but relies on the ERR trap above to manage the
+# EXITCODE for the script.
 set +e
-set -x
 
-export HOME="$WORKSPACE"
-#cudf test
-export LIBCUDF_KERNEL_CACHE_PATH="$WORKSPACE/.cache/rapids/cudf"
-export PATH="/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/local/gcc7/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# FIXME: "source activate" line should not be needed
-source /opt/conda/bin/activate rapids
-env
-nvidia-smi
-conda list
-
-TESTRESULTS_DIR="$WORKSPACE/testresults"
-mkdir -p ${TESTRESULTS_DIR}
-SUITEERROR=0
-
-# run gtests
-for gt in /rapids/cudf/cpp/build/gtests/*; do
-   ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
-   exitcode=$?
-   if (( ${exitcode} != 0 )); then
-      SUITEERROR=${exitcode}
-      echo "FAILED: ${gt}"
-   fi
-done
-
-# Python tests
-export PYTHONPATH=\
-/rapids/cudf/python/cudf:\
-/rapids/cudf/python/dask_cudf:\
-/rapids/cudf/python/custreamz:\
-/rapids/cudf/python/nvstrings:\
-${PYTHONPATH}
-
-cd /rapids/cudf/python/cudf
-py.test -n 6 --junitxml=${TESTRESULTS_DIR}/pytest-cudf.xml -v
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more python tests"
+if (python ${CUGRAPH_ROOT}/ci/utils/is_pascal.py); then
+    echo "WARNING: skipping C++ tests on Pascal GPU arch."
+else
+    echo "C++ gtests for cuGraph..."
+    for gt in tests/*_TEST; do
+        test_name=$(basename $gt)
+        echo "Running gtest $test_name"
+        ${gt} ${GTEST_FILTER} ${GTEST_ARGS}
+        echo "Ran gtest $test_name : return code was: $?, test script exit code is now: $EXITCODE"
+    done
 fi
 
-cd /rapids/cudf/python/dask_cudf
-py.test -n 6 --junitxml=${TESTRESULTS_DIR}/pytest-dask-cudf.xml -v
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more python tests"
-fi
+echo "Python pytest for cuGraph..."
+cd ${CUGRAPH_ROOT}/python
+pytest --cache-clear --junitxml=${CUGRAPH_ROOT}/junit-cugraph.xml -v --cov-config=.coveragerc --cov=cugraph --cov-report=xml:${WORKSPACE}/python/cugraph/cugraph-coverage.xml --cov-report term --ignore=cugraph/raft --benchmark-disable
+echo "Ran Python pytest for cugraph : return code was: $?, test script exit code is now: $EXITCODE"
 
-cd /rapids/cudf/python/custreamz
-py.test -n 6 --junitxml=${TESTRESULTS_DIR}/pytest-custreamz.xml -v
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more python tests"
-fi
+echo "Python benchmarks for cuGraph (running as tests)..."
+cd ${CUGRAPH_ROOT}/benchmarks
+pytest -v -m "managedmem_on and poolallocator_on and tiny" --benchmark-disable
+echo "Ran Python benchmarks for cuGraph (running as tests) : return code was: $?, test script exit code is now: $EXITCODE"
 
-
-#cugraph test
-export LIBCUDF_KERNEL_CACHE_PATH="${WORKSPACE}/.jitcache"
-export PATH="/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/local/gcc7/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# FIXME: "source activate" line should not be needed
-source /opt/conda/bin/activate rapids
-
-# Get datasets
-cd /rapids/cugraph/datasets
-bash ./get_test_data.sh
-export RAPIDS_DATASET_ROOT_DIR=/rapids/cugraph/datasets
-
-# Show environment
-env
-nvidia-smi
-conda list
-
-# Install pytest plugin for cugraph
-conda install rapids-pytest-benchmark
-
-TESTRESULTS_DIR="${WORKSPACE}/testresults"
-mkdir -p ${TESTRESULTS_DIR}
-SUITEERROR=0
-
-# gtests
-for gt in /rapids/cugraph/cpp/build/tests/*_TEST; do
-   # FIXME: remove this ASAP
-   ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
-   exitcode=$?
-   if (( ${exitcode} != 0 )); then
-      SUITEERROR=${exitcode}
-      echo "FAILED: ${gt}"
-   fi
-done
-
-# Python tests
-pytest --ignore=/rapids/cugraph/python/cugraph/raft --junitxml=${TESTRESULTS_DIR}/pytest.xml -v /rapids/cugraph/python
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more tests in /rapids/cugraph/python"
-fi
-
-#cuml test
-
-export CUPY_CACHE_DIR="$WORKSPACE/tmp"
-mkdir -p ${CUPY_CACHE_DIR}
-
-# gtests
-# FIXME: add /rapids/cuml/cpp/build/test/ml_mg when multi-gpus are available!
-for gt in \
-      /rapids/cuml/cpp/build/test/ml \
-      /rapids/cuml/cpp/build/test/prims ; do
-   ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
-   exitcode=$?
-   if (( ${exitcode} != 0 )); then
-      SUITEERROR=${exitcode}
-      echo "FAILED: ${gt}"
-   fi
-done
-
-# Python tests
-py.test --junitxml=${TESTRESULTS_DIR}/pytest.xml -v /rapids/cuml/python/cuml/test -m "not memleak"
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more python tests"
-fi
-
-#cuspatial test
-
-TESTRESULTS_DIR="$WORKSPACE/testresults"
-mkdir -p ${TESTRESULTS_DIR}
-SUITEERROR=0
-
-# Python tests
-cd /rapids/cuspatial
-py.test --junitxml=${TESTRESULTS_DIR}/pytest.xml -v python/cuspatial/cuspatial/tests
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more python tests"
-fi
-#daskcuda
-TESTRESULTS_DIR="$WORKSPACE/testresults"
-mkdir -p ${TESTRESULTS_DIR}
-SUITEERROR=0
-
-# Python tests
-cd /rapids/dask-cuda/dask_cuda
-py.test --junitxml=${TESTRESULTS_DIR}/pytest.xml -v 
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more tests in /rapids/dask-cuda/dask_cuda/tests"
-fi
-
-#Integration.sh test 
-
-set -ex
-export HOME="$WORKSPACE"
-export LIBCUDF_KERNEL_CACHE_PATH="$WORKSPACE/.jitcache"
-export PATH="/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/local/gcc7/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-. /opt/conda/etc/profile.d/conda.sh
-conda activate rapids
-
-gpuci_logger "Show env and current conda list"
-env
-nvidia-smi
-conda list
-
-export TESTRESULTS_DIR="$WORKSPACE/testresults"
-mkdir -p ${TESTRESULTS_DIR}
-SUITEERROR=0
-
-gpuci_logger "Install conda packages needed by tests in rapids environment"
-gpuci_conda_retry --condaretry_max_retries=10 install -y --freeze-installed requests
-
-gpuci_logger "Run Python tests"
-py.test --junitxml=${TESTRESULTS_DIR}/pytest.xml -v "$WORKSPACE/test"
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   gpuci_logger "FAILED: 1 or more tests in $WORKSPACE/test"
-fi
-
-#notebook.sh test 
-set +e
-set -x
-set -o pipefail
-
-export LIBCUDF_KERNEL_CACHE_PATH="$WORKSPACE/.jitcache"
-
-source /opt/conda/bin/activate rapids
-
-# PyTorch is intentionally excluded from our Docker images due
-# to its size, but some notebooks still depend on it.
-case "${CUDA_VER}" in
-"10.1" | "10.2" | "11.0")
-    conda install -y -c pytorch "pytorch=1.7"
-    ;;
-*)
-    echo "Unsupported CUDA version for pytorch."
-    echo "Not installing pytorch."
-    ;;
-esac
-
-
-env
-nvidia-smi
-conda list
-
-/test.sh 2>&1 | tee nbtest.log
-EXITCODE=$?
-python /rapids/utils/nbtestlog2junitxml.py nbtest.log
-
-
-#rmm tests
-
-# gtests
-for gt in /rapids/rmm/build/gtests/*; do
-   ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
-   exitcode=$?
-   if (( ${exitcode} != 0 )); then
-      SUITEERROR=${exitcode}
-      echo "FAILED: ${gt}"
-   fi
-done
-
-# Python tests
-cd /rapids/rmm/python
-py.test --cache-clear --junitxml=${TESTRESULTS_DIR}/rmm_pytest.xml -v
-exitcode=$?
-if (( ${exitcode} != 0 )); then
-   SUITEERROR=${exitcode}
-   echo "FAILED: 1 or more tests in /rapids/rmm/python/tests"
-fi
-
-exit ${SUITEERROR}
+echo "Test script exiting with value: $EXITCODE"
+exit ${EXITCODE}
